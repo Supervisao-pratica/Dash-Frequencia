c051ec1e4fc4e4f121c9975d9ba36e47b93124ad
Supervisao-pratica <tutoria.aprendizagem@pr.senac.br>
2026-07-15 14:41:31 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuracao Web publica do projeto Firebase.
+// A protecao dos dados e feita pelas regras do Firestore.
+window.SENAC_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCr4CpruBzq7F4lOZt4q2KZyJPsdnefkJA",
+    authDomain: "dashfrequencia.firebaseapp.com",
+    projectId: "dashfrequencia",
+    storageBucket: "dashfrequencia.firebasestorage.app",
+    messagingSenderId: "173616886559",
+    appId: "1:173616886559:web:544cdc6075421f57f1b62a",
+    allowedEmailDomain: "@pr.senac.br"
+};
